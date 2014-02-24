5d60a10397ab6775c33469de71a1980dd7b0aa47
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2014-02-24 11:18:41 +0100

    update version destroy submenu

--- app/controllers/admin/submenus_controller.rb
+++ app/controllers/admin/submenus_controller.rb
@@ -45,7 +45,7 @@ module Admin
 
     def destroy
       @submenu.destroy
-      redirect_to admin_title_title_submenu_path(@category.title), notice: "Submenu was successfully destroyed"
+      redirect_to admin_title_title_submenu_index_path(@category.title), notice: "Submenu was successfully destroyed"
     end
 
     private
