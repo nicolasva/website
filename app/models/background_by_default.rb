327bb1ba00570f87b26acd808fd686341dcf560b
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2014-04-16 11:56:40 +0200

    update version website francis ferrari

--- app/models/background_by_default.rb
+++ app/models/background_by_default.rb
@@ -2,6 +2,7 @@ class BackgroundByDefault < ActiveRecord::Base
   before_save :generate_uuid  
   has_one :background, as: :backgroundstyles, dependent: :destroy
   before_save :set_activation_by_default?
+  before_update :set_activation_by_default?
   accepts_nested_attributes_for :background
 
   def self.activation!(activation)
