fc0fbb3947a5ecd675db1f9a1eb8f809063ca26c
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2014-05-30 15:26:44 +0200

    update version website francis ferrari

--- app/models/contents_or_annoncment.rb
+++ app/models/contents_or_annoncment.rb
@@ -1,4 +1,4 @@
 class ContentsOrAnnoncment < ActiveRecord::Base
   belongs_to :contents_or_annoncments, :polymorphic => true
-  CONTENT_OR_ANNONCMENT = {"Annoncment" => 0, "Content" => 1, "Image galery" => 2}
+  CONTENT_OR_ANNONCMENT = {I18n.t("activerecord.attributes.contents_or_annoncment.content_or_annoncment_choice_annoncment") => 0, I18n.t("activerecord.attributes.contents_or_annoncment.content_or_annoncment_choice_content") => 1, I18n.t("activerecord.attributes.contents_or_annoncment.content_or_annoncment_choice_galery_image") => 2}
 end
