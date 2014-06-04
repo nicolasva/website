34994f048fccc195483f74f6fc3d797b5fce8393
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2014-06-04 13:45:31 +0200

    update version website francis ferrari

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,40 +31,28 @@
                           :uuid => UUID.new.generate,
                           :title => "Menu_background by default",
                           :activation => true,
-                          :align_attributes => { :align => 1 },
+                          :align_attributes => { :align => 0 },
                           :background_attributes => { :border => 0, :border_style => "solid", :no_background_image => 1, :background_image_cache => "", :no_repeat => 0, :border_color => "transparent"}
                         })
 
-  Rails.logger.info @menu_background.id
 
-  Align.create({
-                :align => 0,
-                :aligns_id => @menu_background.id,
-                :aligns_type => "MenuBackground"
-              })
+  #@sub_menu_background = SubMenuBackground.create({
+  #                          :uuid => UUID.new.generate,
+  #                          :title => "sub_menu_background by default",
+  #                          :activation => false,
+  #                          :align_attributes => { :align => 0 },
+  #                          :background_attributes => { :border => 0, :border_style => "solid", :no_background_image => 1, :background_image_cache => "", :no_repeat => 0, :border_color => "transparent"}
+  #                         })
 
-  @sub_menu_background = SubMenuBackground.create({
-                            :uuid => UUID.new.generate,
-                            :title => "sub_menu_background by default",
-                            :activation => true,
-                            :align_attributes => { :align => 1 },
-                            :background_attributes => { :border => 0, :border_style => "solid", :no_background_image => 1, :background_image_cache => "", :no_repeat => 0, :border_color => "transparent"}
-                           })
-
-  Align.create({
-                :align => 0,
-                :aligns_id => @sub_menu_background.id,
-                :aligns_type => "SubMenuBackground"
-              })
-  FooterBackground.create({
-                          :uuid => UUID.new.generate,
-                          :title => "home_page_submenu by default",
-                          :activation => true,
-                          :background_attributes => { :border => 0, :border_style => "solid", :no_background_image => 1, :background_image_cache => "", :no_repeat => 0, :border_color => "transparent"}
-                          })
-  HeaderBackground.create({
-                          :uuid => UUID.new.generate,
-                          :title => "Header Background by default",
-                          :activation => true,
-                          :background_attributes => { :border => 0, :border_style => "solid", :no_background_image => 1, :background_image_cache => "", :no_repeat => 0, :border_color => "transparent"}
-                          })
+  #FooterBackground.create({
+  #                        :uuid => UUID.new.generate,
+  #                        :title => "home_page_submenu by default",
+  #                        :activation => true,
+  #                        :background_attributes => { :border => 0, :border_style => "solid", :no_background_image => 1, :background_image_cache => "", :no_repeat => 0, :border_color => "transparent"}
+  #                        })
+  #HeaderBackground.create({
+  #                        :uuid => UUID.new.generate,
+  #                        :title => "Header Background by default",
+  #                        :activation => true,
+  #                        :background_attributes => { :border => 0, :border_style => "solid", :no_background_image => 1, :background_image_cache => "", :no_repeat => 0, :border_color => "transparent"}
+  #                        })
