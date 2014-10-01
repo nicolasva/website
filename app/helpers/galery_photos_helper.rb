0c6f620f076ed8271afcf9177d30e0097f7c2960
Nicolas VANDENBOGAERDE <nvandenbogaerde@dotgee.fr>
2014-10-01 16:47:48 +0200

    update version website francis

--- app/helpers/galery_photos_helper.rb
+++ app/helpers/galery_photos_helper.rb
@@ -1,13 +1,13 @@
 module GaleryPhotosHelper
   def edit_admin_galery_photo_path(galery_photo)
-    return @submenu.nil? ? edit_admin_title_galery_photo_path(@category.title, galery_photo.uuid) : edit_admin_title_title_submenu_galery_photo_path(@category.title, @submenu.title, galery_photo.uuid)
+    return @submenu.nil? ? edit_admin_title_galery_photo_path(@category.title, galery_photo.uuid) : edit_admin_title_title_submenu_galery_photo_path(@category.category.title, @submenu.title, galery_photo.uuid)
   end
 
   def destroy_admin_galery_photo_path(galery_photo)
-    return @submenu.nil? ? admin_title_galery_photo_path(@category.title, galery_photo.uuid) : admin_title_title_submenu_galery_photo_path(@category.title, @submenu.title, galery_photo.uuid)
+    return @submenu.nil? ? admin_title_galery_photo_path(@category.title, galery_photo.uuid) : admin_title_title_submenu_galery_photo_path(@category.category.title, @submenu.title, galery_photo.uuid)
   end
 
   def new_admin_galery_photo_path
-    return @submenu.nil? ? new_admin_title_galery_photo_path(@category.title) : new_admin_title_title_submenu_galery_photo_path(@category.title, @submenu.title)
+    return @submenu.nil? ? new_admin_title_galery_photo_path(@category.title) : new_admin_title_title_submenu_galery_photo_path(@category.category.title, @submenu.title)
   end
 end
